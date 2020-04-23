f04e0216b605585d16f5e6b3f189dd1254165003
Long Liang <lngr23@gmail.com>
2020-04-23 12:07:21 -0700

    added mutation for creating votes.

--- links/schema.py
+++ links/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Link
+from .models import Link, Vote
 from users.schema import UserType
 
 
@@ -54,8 +54,33 @@ class CreateLink(graphene.Mutation):
     )
 
 
+class CreateVote(graphene.Mutation):
+  user = graphene.Field(UserType)
+  link = graphene.Field(LinkType)
+
+  class Arguments:
+    link_id = graphene.Int()
+
+  def mutate(self, info, link_id):
+    user = info.context.user
+    if user.is_anonymous:
+      raise Exception('You must be logged in to vote')
+
+    link = Link.objects.filter(id=link_id).first()
+    if not link:
+      raise Exception('Invalid Link')
+
+    Vote.objects.create(
+      user=user,
+      link=link
+    )
+
+    return CreateVote(user=user, link=link)
+
+
 class LinkMutation(graphene.ObjectType):
   """
   Field to be resolved.
   """
   create_link = CreateLink.Field()
+  create_vote = CreateVote.Field()
