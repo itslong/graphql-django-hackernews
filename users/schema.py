45ef6284bfd0c11adeb901030e8b33bed8a71c1e
Long Liang <lngr23@gmail.com>
2020-04-22 22:49:01 -0700

    added query to test for auth.

--- users/schema.py
+++ users/schema.py
@@ -10,9 +10,17 @@ class UserType(DjangoObjectType):
 
 class UserQuery(graphene.ObjectType):
   users = graphene.List(UserType)
+  me = graphene.Field(UserType)
 
   def resolve_users(self, info):
     return get_user_model().objects.all()
+
+  def resolve_me(self, info):
+    user = info.context.user
+    if user.is_anonymous:
+      raise Exception('Not logged in')
+
+    return user
 
 
 class CreateUser(graphene.Mutation):
