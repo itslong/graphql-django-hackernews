609e1af8e85a24ce51e1cc7e15e893e29e3c6d5f
Long Liang <lngr23@gmail.com>
2020-04-23 13:03:49 -0700

    added search as optional arg to query.

--- links/schema.py
+++ links/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
+from django.db.models import Q
 
 from .models import Link, Vote
 from users.schema import UserType
@@ -17,10 +18,17 @@ class VoteType(DjangoObjectType):
 
 
 class LinkQuery(graphene.ObjectType):
-  links = graphene.List(LinkType)
+  links = graphene.List(LinkType, search=graphene.String())
   votes = graphene.List(VoteType)
 
-  def resolve_links(self, info, **kwargs):
+  def resolve_links(self, info, search=None, **kwargs):
+    if search:
+      filtered = (
+          Q(url__icontains=search) |
+          Q(description__icontains=search)
+      )
+      return Link.objects.filter(filtered)
+    
     return Link.objects.all()
 
   def resolve_votes(self, info, **kwargs):
